de8f767311285d735a8838c55b055dfb9615e952
wqdchn <wqdong.chn@gmail.com>
2019-05-16 19:03:23 +0800

    添加了中等题有序矩阵返回指定排序位置的数值

---
+++ src/medium/Kth_Smallest_Element_in_a_Sorted_Matrix.java
@@ -0,0 +1,59 @@
+package src.medium;
+
+import java.util.*;
+
+/**
+ * @program: leetcode
+ * @description: https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+ * @author: wqdong
+ * @create: 2019-05-16 18:36
+ * Given a n x n matrix where each of the rows and columns are sorted in ascending order,
+ *
+ * find the kth smallest element in the matrix.
+ *
+ * Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+ *
+ * Example:
+ *
+ * matrix = [
+ *    [ 1,  5,  9],
+ *    [10, 11, 13],
+ *    [12, 13, 15]
+ * ],
+ * k = 8,
+ *
+ * return 13.
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ n2.
+ **/
+public class Kth_Smallest_Element_in_a_Sorted_Matrix {
+
+    public static void main(String[] args){
+        int[][] matrix = {{1,  5,  9}, {10, 11, 13},{12, 13, 15}};
+        int k = 8;
+
+        //打印二维数组
+        for(int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix.length; j++)
+                System.out.print(matrix[i][j]+" ");
+            System.out.println();
+        }
+
+        System.out.println("第"+k+"大的数是"+kthSmallest(matrix, k));
+
+    }
+
+    public static int kthSmallest(int[][] matrix, int k) {
+        int output = 0;
+        List<Integer> matlist = new ArrayList();
+
+        for(int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix.length; j++)
+                matlist.add(matrix[i][j]);
+            System.out.println();
+        }
+
+        Collections.sort(matlist);
+        return matlist.get(k-1);
+    }
+}
