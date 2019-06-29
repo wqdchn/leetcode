5ed40f9be99d2c69cb07b803091efa1e1354af23
wqdchn <wqdong.chn@gmail.com>
2019-06-29 11:40:22 +0800

    添加了中等题字符串乘法+medium:mutiply_strings

---
+++ src/medium/Mutiply_strings.java
@@ -0,0 +1,57 @@
+package src.medium;
+
+/**
+ * @program: leetcode
+ * @description: Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+ *
+ * Example 1:
+ *
+ * Input: num1 = "2", num2 = "3"
+ * Output: "6"
+ * Example 2:
+ *
+ * Input: num1 = "123", num2 = "456"
+ * Output: "56088"
+ * Note:
+ *
+ * The length of both num1 and num2 is < 110.
+ * Both num1 and num2 contain only digits 0-9.
+ * Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+ * You must not use any built-in BigInteger library or convert the inputs to integer directly.
+ * @author: wqdong
+ * @create: 2019-06-29 10:49
+ **/
+public class Mutiply_strings {
+
+    public static void main(String[] args){
+        String num1 = "123";
+        String num2 = "456";
+        System.out.println(multiply(num1, num2));
+    }
+
+    public static String multiply(String num1, String num2) {
+        num1 = new StringBuilder(num1).reverse().toString();//逆序以规避开头0
+        num2 = new StringBuilder(num2).reverse().toString();
+
+        int[] d = new int[num1.length() + num2.length()];
+        for (int i = 0; i < num1.length(); i++) {
+            int a = num1.charAt(i) - '0';
+            for (int j = 0; j < num2.length(); j++) {
+                int b = num2.charAt(j) - '0';
+                d[i + j] += a * b;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < d.length; i++) {
+            int digit = d[i] % 10; //
+            int carry = d[i] / 10; //用于进位
+            sb.insert(0, digit);
+            if (i < d.length - 1)
+                d[i + 1] += carry;
+        }
+        while (sb.length() > 0 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
