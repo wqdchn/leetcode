dd6fbd87d7b85149b88ca5a3922a4b8a86987def
wqdchn <wqdong.chn@gmail.com>
2019-08-15 20:27:05 +0800

    算法40讲+树第二题+中等题二叉树最近公共祖先

---
+++ src/medium/Lowest_common_ancestor_of_a_binary_tree.java
@@ -0,0 +1,53 @@
+package src.medium;
+
+/**
+ * @program: leetcode
+ * @description: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+ * @author: wqdong
+ *
+ * Example 1:
+ *
+ * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * Output: 3
+ * Explanation: The LCA of nodes 5 and 1 is 3.
+ *
+ * @create: 2019-08-15 20:01
+ **/
+public class Lowest_common_ancestor_of_a_binary_tree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public static void main(String[] args){
+
+        TreeNode root = new TreeNode(3);
+
+        root.left = new TreeNode(5);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.left.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(4);
+
+        root.right = new TreeNode(1);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+
+        TreeNode p = root.left;
+        TreeNode q = root.right;
+
+        System.out.print(lowestCommonAncestor(root, p, q).val);
+
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        return left == null ? right : right == null ? left : root;
+    }
+
+}
