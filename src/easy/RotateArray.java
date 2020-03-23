553bc66582e6470a4807e91dfda4a952a72c578e
wqdchn <wqdong.chn@gmail.com>
2020-03-23 08:21:52 +0800

    算法40讲课外练习+数组+189.Rotate Array

--- src/easy/RotateArray.java
+++ src/easy/RotateArray.java
@@ -6,12 +6,12 @@ package src.easy;
  * @author: wqdong
  * @create: 2020-02-10 07:50
  **/
-public class rotate_array {
+public class RotateArray {
 
   public static void main(String[] args) {
     int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7};
     int k = 3;
-    rotate(nums, k);
+    rotate2(nums, k);
     for (int num : nums) {
       System.out.print(num + " ");
     }
@@ -25,6 +25,24 @@ public class rotate_array {
     }
     for (int i = 0; i < nums.length; i++) {
       nums[i] = a[i];
+    }
+  }
+
+  // Runtime: 0 ms, faster than 100.00% of Java online submissions for Rotate Array.
+  public static void rotate2(int[] nums, int k) {
+    k %= nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+  }
+
+  private static void reverse(int[] nums, int left, int right) {
+    while (left < right) {
+      int temp = nums[left];
+      nums[left] = nums[right];
+      nums[right] = temp;
+      left++;
+      right--;
     }
   }
 
